20aa0ed737b7ff40f5845ed07c45fa3cf66759fe
TIANCHENG <306489696@QQ.COM>
2020-03-15 13:53:00 -0700

    9

--- Wumpus_World_Python_Shell/src/MyAI.py
+++ Wumpus_World_Python_Shell/src/MyAI.py
@@ -97,7 +97,7 @@ class MyAI ( Agent ):
             return Agent.Action.GRAB
 
         for position in self.__traveledplace:
-            if self.__traveledplace.count(position) >= 11:
+            if self.__traveledplace.count(position) >= 9:
                 self.__grabbed = True
                 
         
