a854b9477c887c868b027216964a22fed46ff4b0
TIANCHENG <306489696@QQ.COM>
2020-03-15 15:50:18 -0700

    start 7

--- Wumpus_World_Python_Shell/src/MyAI.py
+++ Wumpus_World_Python_Shell/src/MyAI.py
@@ -73,7 +73,7 @@ class MyAI ( Agent ):
 
         if self.__myPosition == (0,0):
             self.__timesInStart += 1
-        if self.__timesInStart >= 6:
+        if self.__timesInStart >= 7:
             return Agent.Action.CLIMB
 
         ##breeze condition
